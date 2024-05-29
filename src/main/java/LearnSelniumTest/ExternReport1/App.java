891446fad74352fc3b6c49ef97a285e93ac6833d
Naresh-Aspire <nareshkumaroctober10@gmail.com>
2024-05-29 12:12:52 +0530

    commit from GitX user

--- src/main/java/LearnSelniumTest/ExternReport1/App.java
+++ src/main/java/LearnSelniumTest/ExternReport1/App.java
@@ -12,5 +12,12 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        
+        
+        System.out.println( "Git hub comments here");
+        System.out.println( "Git hub comments here");
+        
+        
+        
     }
 }
---
+++ src/main/java/LearnSelniumTest/ExternReport1/testing.java
@@ -0,0 +1,10 @@
+package LearnSelniumTest.ExternReport1;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "web automation" );
+	}
+
+}
